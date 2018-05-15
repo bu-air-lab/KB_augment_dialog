882559774e45013b61d61d4774b2883fa3303bab
Sujay <sujaymanb@gmail.com>
2018-05-15 13:39:23 -0400

    fixed figure legend and colors

--- graphs/survey.py
+++ graphs/survey.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 from matplotlib import mlab
+from matplotlib.patches import Patch
 import numpy as np
 import pandas as pd
 
@@ -23,26 +24,39 @@ for i in range(len(trial1)):
     t2 = []
     # count each value (0-4)
     for j in range(5):
-        t1.append(trial1[i].count(j)/n)
-        t2.append(trial2[i].count(j)/n)
+        t1.append((trial1[i].count(j)/n) * 100)
+        t2.append((trial2[i].count(j)/n) * 100)
 
     d1.append(t1)
     d2.append(t2)
 
-colors = ['firebrick','lightcoral','gainsboro','cornflowerblue', 'darkblue']
+colors = ['firebrick','indianred','lightcoral','mistyrose','gainsboro']
 cols = ['Strongly Disagree', 'Disagree', 'Neutral', 'Agree', 'Strongly Agree']
 questions = ['The tasks were easy to understand',
         'The robot understood me',
         'The robot frustrated me',
-        'I would use the robot to get items for myself or other']
+        'I would use the robot to\n get items for myself or other']
 keys = ['Q1','Q2','Q3','Q4']
+legend_elements = [Patch(facecolor='firebrick', label='Strongly Disagree'),
+                   Patch(facecolor='indianred', label='Disagree'),
+                   Patch(facecolor='lightcoral', label='Neutral'),
+                   Patch(facecolor='mistyrose', label='Agree'),
+                   Patch(facecolor='gainsboro', label='Strongly Agree'),
+                   Patch(hatch='...', label='Our Method'),
+                   Patch(hatch='\\\\', label='Baseline')]
 
-df1 = pd.DataFrame(data=d1, columns=cols, index=keys)
-df2 = pd.DataFrame(data=d2, columns=cols, index=keys)
+df1 = pd.DataFrame(data=d1, columns=cols, index=questions)
+df2 = pd.DataFrame(data=d2, columns=cols, index=questions)
 
-fig, ax = plt.subplots()
-df1.plot.barh(width=0.25, position=0, stacked=True, color=colors, ax=ax, edgecolor='black')
-df2.plot.barh(width=0.25, position=1, stacked=True, color=colors, ax=ax, edgecolor='black', legend=False)
+fig, ax = plt.subplots(figsize=(9,6))
+df1.plot.barh(hatch='...', width=0.25, position=0, stacked=True, color=colors, ax=ax, edgecolor='black',  legend=False)
+df2.plot.barh(hatch='\\\\', width=0.25, position=1, stacked=True, color=colors, ax=ax, edgecolor='black', legend=False)
+
+ax.legend(handles=legend_elements, loc='2', bbox_to_anchor=(1.05,1), borderaxespad=0.)
+plt.yticks(clip_on=False)
+plt.xlabel('Participants (%)')
+plt.subplots_adjust(left=0.33, right=0.75)
 
 #plt.margins(0.5)
 plt.show()
+fig.savefig('survey.svg')
